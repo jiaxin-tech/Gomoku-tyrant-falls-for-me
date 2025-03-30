2e3da978a5363d97db78d6dde37256a37da512d1
jiaxin-tech <jiaxin63@icloud.com>
2025-03-30 21:05:19 +0800

    day4
    today end at 250 (lol)
    the remaining is:
    1.count the max black/white score
    2.finish the undo/redo function
    3.if possible(have enough time), optimize  user experience .like the font, color, theme, pop-up and so on.

--- HelloApplication.java
+++ HelloApplication.java
@@ -1,6 +1,11 @@
 package com.gomoku;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -8,8 +13,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
@@ -21,15 +32,37 @@ public class HelloApplication extends Application {
     private int [][]  board=new int[Size][Size];
     public boolean blackturn=true;
     public boolean gameover=false;
+    public int count=0;
+    public int blackmax=0;
+    public int whitemax=0;
 
     private GraphicsContext gc;
     private Label label;
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane root = new BorderPane();
+        StackPane stackpane = new StackPane();
         Canvas canvas = new Canvas(Size*cellSize,Size*cellSize);
         gc = canvas.getGraphicsContext2D();
-        drawboard();
+
+        Label welcomelable = new Label("Welcome to Gomoku!");
+        welcomelable.setFont(Font.font("Arial", FontWeight.BOLD,50));
+        Button playButton = new Button("Play");
+        playButton.setPrefWidth(200);
+        playButton.setPrefHeight(50);
+        playButton.setOnAction(event -> {
+            root.setCenter(stackpane);
+            board = new int[Size][Size];
+            blackturn = true;
+            gameover = false;
+            label.setText("Current Player: Black");
+            drawboard();
+        });
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(welcomelable,playButton);
+        vbox.setAlignment(Pos.CENTER);
+        root.setCenter(vbox);
+
 
         HBox hbox = new HBox(12);
         Button startButton = new Button("New Game");
@@ -40,21 +73,86 @@ public class HelloApplication extends Application {
             label.setText("Current Player: Black");
             drawboard();
         });
+        Button undoButton = new Button("Undo");
+        undoButton.setOnAction(event -> {
+            undo();
+        });
+        Button redoButton = new Button("Redo");
+        redoButton.setOnAction(event -> {
+            redo();
+        });
+        Button exitButton = new Button("Exit");
+        exitButton.setOnAction(event -> {
+            root.setCenter(vbox);
+        });
         label = new Label("Current Player: Black");
-        hbox.getChildren().addAll(startButton, label);
+        label.setFont(Font.font("Arial", FontWeight.BOLD,30));
+        hbox.getChildren().addAll(startButton, label, redoButton, undoButton,exitButton);
+        hbox.setAlignment(Pos.CENTER);
 
-        canvas.setOnMouseClicked(event -> {
+        HBox hbox2 = new HBox(12);
+        hbox2.setAlignment(Pos.CENTER);
+        Label countlabel = new Label("Number of movements:"+count);
+        countlabel.setFont(Font.font("Arial", FontWeight.BOLD,24));
+        Label black = new Label("Max black score:"+blackmax);
+        black.setFont(Font.font("Arial", FontWeight.BOLD,24));
+        Label white = new Label("Max white score:"+whitemax);
+        white.setFont(Font.font("Arial", FontWeight.BOLD,24));
+        hbox2.getChildren().addAll(countlabel,black,white);
+        root.setTop(hbox2);
+
+        Canvas usercanvas = new Canvas(Size*cellSize,Size*cellSize);
+        GraphicsContext gc2 = usercanvas.getGraphicsContext2D();
+        stackpane.getChildren().addAll(canvas,usercanvas);
+
+        int [][] highlightpoint=new int[Size*Size][2];
+        for(int i=0; i<Size; i++){
+            for(int j=0; j<Size; j++){
+                highlightpoint[i*Size+j][0]=i*cellSize;
+                highlightpoint[i*Size+j][1]=j*cellSize;
+            }
+        }
+        usercanvas.addEventHandler(MouseEvent.MOUSE_MOVED,event -> {
+            Color highlightColor = Color.YELLOW;
+            double highlightRadius = 10;
+            gc2.clearRect(0,0,Size*cellSize,Size*cellSize);
+            for (int[] point : highlightpoint) {
+                int x = point[0];
+                int y = point[1];
+                if (Math.hypot(event.getX() - x, event.getY() - y) <= highlightRadius) {
+                    gc2.setFill(highlightColor);
+                    gc2.fillOval(x - highlightRadius, y - highlightRadius, highlightRadius * 2, highlightRadius * 2);
+                }
+
+                int oordinatex= (int) Math.round(event.getX()/cellSize);
+                int oordinatey= (int) Math.round(event.getY()/cellSize);
+                if(board[oordinatex][oordinatey]!=0){
+                    gc2.setFill(Color.RED);
+                    gc2.fillOval(oordinatex*cellSize- highlightRadius, oordinatey*cellSize- highlightRadius, highlightRadius * 2, highlightRadius * 2);
+                }
+            }
+        });
+        usercanvas.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            canvas.fireEvent(event);
+        });
+
+        canvas.addEventHandler(MouseEvent.MOUSE_CLICKED,event -> {
            if(!gameover) {
                handleClick(event.getX(),event.getY());
            }
 
         });
-        root.setCenter(canvas);
         root.setBottom(hbox);
 
-        stage.setScene(new Scene(root));
+        stage.setScene(new Scene(root,800,800));
         stage.setTitle("Gomoku");
         stage.show();
+    }
+
+    private void redo() {
+    }
+
+    private void undo() {
     }
 
     private void handleClick(double x, double y) {
@@ -63,10 +161,11 @@ public class HelloApplication extends Application {
         if(board[clickedX][clickedY]==0) {
             board[clickedX][clickedY]=blackturn ? 1 : 2;
             drawStone(clickedX,clickedY,blackturn? Color.BLACK : Color.WHITE);
+            count++;
 
             if(checkwin(clickedX,clickedY)) {
                 gameover = true;
-                label.setText("Game Over"+" "+(blackturn? "Black" : "White")+" "+"win!");
+                label.setText("Game Over"+"     "+(blackturn? "Black" : "White")+" "+"win!");
             }else {
                 blackturn=!blackturn;
                 label.setText("Current Player:"+(blackturn? "Black" : "White"));
@@ -107,6 +206,12 @@ public class HelloApplication extends Application {
     }
 
     private void drawboard() {
+        FadeTransition boardfadein=new FadeTransition(Duration.seconds(3), gc.getCanvas());
+        boardfadein.setFromValue(0.0);
+        boardfadein.setToValue(1.0);
+        boardfadein.setCycleCount(1);
+        boardfadein.setAutoReverse(false);
+        boardfadein.play();
         gc.setFill(Color.BURLYWOOD);
         gc.fillRect(0,0,Size*cellSize,Size*cellSize);
         gc.setStroke(Color.BLACK);
@@ -127,8 +232,17 @@ public class HelloApplication extends Application {
     }
 
     private void drawStone(int i, int j, Color color) {
-        gc.setFill(color);
-        gc.fillOval(i*cellSize- (double) cellSize /2,j*cellSize- (double) cellSize /2,cellSize,cellSize);
+        Timeline timeline = new Timeline();
+        for(int c=1;c<10;c++){
+            double opacity=c/10.0;
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(c*100), event -> {
+                gc.setFill(Color.color(color.getRed(),color.getGreen(),color.getBlue(),opacity));
+                gc.fillOval(i*cellSize- (double) cellSize /2,j*cellSize- (double) cellSize /2,cellSize,cellSize);
+            });
+            timeline.getKeyFrames().add(keyFrame);
+        }
+        timeline.setCycleCount(1);
+        timeline.play();
     }
 
     public static void main(String[] args) {
